61d7f7baeffdbddd9df431095cf1a0ed1ee79adf
Rafael-devops <rafaelllcomputadores892@gmail.com>
2021-12-21 00:53:37 -0300

    alterações routes

--- app/Http/Controllers/Subscription/SubscriptionController.php
+++ app/Http/Controllers/Subscription/SubscriptionController.php
@@ -5,7 +5,7 @@ namespace App\Http\Controllers\Subscriptions;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
-class SubscriptionsController extends Controller
+class SubscriptionController extends Controller
 {
     public function __contruct(){
 
